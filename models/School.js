9a5255d7bf55eefc19aa4a513048741ee8ad9f73
TerrillL <31742639+TerrillL@users.noreply.github.com>
2018-11-08 11:28:57 -0600

    Updated Schemas

--- models/School.js
+++ models/School.js
@@ -4,21 +4,20 @@ var Schema = mongoose.Schema;
 
 //const School = new mongoose.Schema({
 const schoolSchema = new Schema({
-    name: {type:String, trim:true, default:''},
-    address: {type:String, trim:true, default:''},
-    city: {type:String, trim:true, default:''},
-    state: {type:String, trim:true, default:''},
-    usdCode: {type:Number, default: 0},
-    email: {type:String, trim:true, default:''},
-    phone: {type:String, trim:true, default:''}
+    name: {type:String, trim:true, required: true},
+    address: {type:String, trim:true, required: true},
+    city: {type:String, trim:true, required: true},
+    state: {type:String, trim:true, required: true},
+    usdCode: {type:Number, default: 0, required: true},
+    email: {type:String, trim:true, required: false},
+    phone: {type:String, trim:true, required: false}
+});
+
+schoolSchema
+.virtual('url')
+.get(function () {
+return '/api/school' + this._id;
 });
 
 //module.exports = mongoose.model('School', School)
 module.exports = mongoose.model('School', schoolSchema)
-
-
-//Author
-//.virtual('name')
-//.get(function() {
-//    return name
-//})
--- models/Student.js
+++ models/Student.js
@@ -4,16 +4,28 @@ var Schema = mongoose.Schema;
 
 //const Student = new mongoose.Schema({
 const studentSchema = new Schema({
-    firstName: {type:String, trim:true, default:''},
-    lastName: {type:String, trim:true, default:''},
-    address: {type:String, trim:true, default:''},
-    city: {type:String, trim:true, default:''},
-    state: {type:String, trim:true, default:''},
-    school: {type:String, trim:true, default:''},
-    //school: {type: Schema.Types.ObjectId, ref: 'School'},
-    gradeLevel: {type:Number, default:0},
-    email: {type:String, trim:true, default:''},
-    phone: {type:String, trim:true, default:''}
+    firstName: {type:String, trim:true, required: true},
+    lastName: {type:String, trim:true, required: true},
+    address: {type:String, trim:true, required: true},
+    city: {type:String, trim:true, required: true},
+    state: {type:String, trim:true, required: true},
+    //school: {type:String, trim:true, required: true},
+    school: {type: Schema.Types.ObjectId, ref: 'School', required: true},
+    gradeLevel: {type:Number, default:0, required: true, max: 12},
+    email: {type:String, trim:true, required: false},
+    phone: {type:String, trim:true, required: false}
+});
+
+studentSchema
+.virtual('name')
+.get(function() {
+    return this.lastName + ", " + this.firstName;
+});
+
+studentSchema
+.virtual('url')
+.get(function() {
+    return '/api/student/' + this._id;
 });
 
 //module.exports = mongoose.model('Student', Student)
